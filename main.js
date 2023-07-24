8edac6ec71db4a6e8741174241d48a907c92a684
Maximkooo <43643606+Maximkooo@users.noreply.github.com>
2023-07-24 14:10:50 +0200

    init code and HTML

---
+++ main.js
@@ -0,0 +1,23 @@
+import express from "express";
+import { engine } from "express-handlebars";
+
+const app = express()
+
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.set('views', './views')
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (_, res) => {
+  res.render('index')
+})
+
+app.post('/', (req, res) => {
+  const prompt = req.body.prompt
+  const size = req.body.size
+  const number = req.body.number ?? 1
+  console.log(prompt, size, number);
+  res.render('index')
+})
+
+app.listen(3000, () => console.log('Server started'))
